1d74ee548304b316daf672d960ec74fbc11a6b9c
Asif <asif.mhaldar03@gmail.com>
2026-01-30 11:46:58 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -16,9 +16,33 @@ app.use("/api/movies", movieRoutes);
 app.use("/api/auth", authRoutes);
 
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+let isConnected = false;
+async function connectToMongoDB(){
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    isConnected = true;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("MongoDB connection error:", error);
+  }
+}
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(async (req, res, next) => {
+  if (!isConnected) {
+    await connectToMongoDB();
+  }
+  next();
+}
+);
+
+module.exports = app;
+
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDB Connected"))
+//   .catch(err => console.log(err));
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
