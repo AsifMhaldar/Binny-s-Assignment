d25b9345c73887da4f684ae70449e7d753d78ae5
Asif <asif.mhaldar03@gmail.com>
2026-01-30 12:03:03 +0530

    url change

--- Frontend/src/services/api.jsx
+++ Frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "binny-s-assignment.vercel.app/api"
 });
 
 API.interceptors.request.use((req) => {
