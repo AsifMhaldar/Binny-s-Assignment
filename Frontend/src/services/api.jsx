9572ca1dd8eac8fdf10de98dc42e1fb0cd2cacfe
Asif <asif.mhaldar03@gmail.com>
2026-01-31 14:12:39 +0530

    add backend url

--- Frontend/src/services/api.jsx
+++ Frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://mern-movie-app-iztq.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
